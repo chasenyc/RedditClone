88043cc0cef6ec645d2bb35c70c8f863a85e9ba8
AlexAndPat <AlexAndPat@appacademy.io>
2015-10-23 12:25:01 -0400

    Generate Subs table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151023130103) do
+ActiveRecord::Schema.define(version: 20151023162044) do
+
+  create_table "subs", force: :cascade do |t|
+    t.string   "title",       null: false
+    t.text     "description", null: false
+    t.integer  "user_id",     null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "subs", ["user_id"], name: "index_subs_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",           null: false
